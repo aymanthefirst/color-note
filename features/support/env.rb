53364cd87b95afc973e0b5f9af6263902cc372fe
FarhaanSG <fahmed@spartaglobal.com>
2018-10-29 11:38:02 +0000

    changed location of app in env file

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,7 +7,7 @@ def opts
     caps: {
     platformName: "Android",
     deviceName: "emulator-5554",
-    app: "C:\\Users\\TECH-W80.LAPTOP-HHQKO8FF\\Downloads\\colorNote.apk"
+    app: "C:\\Users\\TECH-W85\\Downloads\\colorNote.apk"
   }
 }
 
