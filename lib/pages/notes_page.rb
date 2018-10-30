4d0adfb06aeb691ad55b7499bacb45fb34d765e4
aymanthefirst <AHarake@spartaglobal.com>
2018-10-30 15:51:25 +0000

    pretty much done

--- lib/pages/notes_page.rb
+++ lib/pages/notes_page.rb
@@ -4,6 +4,13 @@ class NotesPage
     @driver = driver
   end
 
-  
+  def click_add_note
+        @driver.find_element(:id, "com.socialnmobile.dictapps.notepad.color.note:id/empty").click
+  end
+
+  def click_add_text_note
+
+    @driver.find_element(:id, "com.socialnmobile.dictapps.notepad.color.note:id/text").click
+  end
 
 end
