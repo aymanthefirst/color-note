e91435072f94f6b5e4ddee4a4f051a1dc4d1b350
aymanthefirst <AHarake@spartaglobal.com>
2018-10-31 13:40:38 +0000

    dan asked to test

--- features/step_definitions/search_bar_step_defs.rb
+++ features/step_definitions/search_bar_step_defs.rb
@@ -1,24 +1,35 @@
-Given("I am on the search page") do
-  tutorial.click_skip
+Given("A valid note exists") do
+  notes_page.skip_tutorial
+  notes_page.click_add_note
+  notes_page.click_add_text_note
+  notes_page.input_note_title "Example title"
+  notes_page.input_note_info "Example note description"
+  notes_page.set_note_colour_purple
+  notes_page.click_back_navigation # closing keyboard
+  notes_page.click_back_navigation # KEYCODE_BACK
+  notes_page.click_back_navigation # KEYCODE_BACK
+end
+
+And("I am on the search page") do
   tutorial.click_menu
   search_bar_page.click_search_page
-  search_bar_page.fill_search_bar
 end
 
 When("I type valid note details in the search bar") do
-  pending # Write code here that turns the phrase above into concrete actions
+  search_bar_page.fill_search_bar
 end
 
 Then("I should get the valid results") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(search_bar_page.check_for_example_title).to eq("example title")
 end
 
 Given("I am on the the search page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  tutorial.click_menu
+  search_bar_page.click_search_page
 end
 
 When("I type invalid note details in the search bar") do
-  pending # Write code here that turns the phrase above into concrete actions
+  search_bar_page.fill_search_bar
 end
 
 Then("I should receive an error for not finding the product") do
