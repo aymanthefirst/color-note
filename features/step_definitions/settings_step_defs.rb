1b1d727ef4d515c601bd6c70e6c169dfb7fe50a1
FarhaanSG <fahmed@spartaglobal.com>
2018-10-31 09:51:43 +0000

    added more feature test for settings page

--- features/step_definitions/settings_step_defs.rb
+++ features/step_definitions/settings_step_defs.rb
@@ -1,12 +1,12 @@
+#TEST TO SLEECT DEFAULT SCREEN
 When("I click on the settings link") do
-  expect(settings_page.click_settings_link).to eq(true)
+  settings_page.click_settings_link
 end
 
 Then("I should be able to change default page to calendar") do
-  expect(settings_page.click_new_default_option).to eq(true)
+  settings_page.click_default_screen
+  settings_page.click_new_default_option
   sleep 2
-  $driver.closeApp
-  $driver.launchApp
-  sleep 2
-
 end
+
+#TEST TO CHANGE NOTE COLOUR
--- lib/pages/settings_page.rb
+++ lib/pages/settings_page.rb
@@ -5,15 +5,19 @@ class SettingsPage
   end
 
   def click_settings_link
-    @driver.find_elements(:class, "android.widget.LinearLayout")[5].click
+    @driver.find_elements(:class, "android.widget.LinearLayout")[12].click
   end
 
   def click_default_screen
-    @driver.find_elements(:class, "android.widget.LinearLayout")[6].click
+    @driver.find_elements(:class, "android.widget.LinearLayout")[7].click
   end
 
   def click_new_default_option
     @driver.find_elements(:class, "android.widget.CheckedTextView").last.click
+  end
+
+  def click_default_color
+    @driver.find_element(:id, "com.socialnmobile.dictapps.notepad.color.note:id/btn5").click
   end
 
 end
