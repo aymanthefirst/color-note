2be9fda7b9cd66fb55e7e6ff348d703c36681a35
aymanthefirst <AHarake@spartaglobal.com>
2018-11-01 16:02:05 +0000

    log in is done

--- features/step_definitions/log_in_step_def.rb
+++ features/step_definitions/log_in_step_def.rb
@@ -2,7 +2,6 @@ Given("I access the facebook sign in page") do
   notes_page.skip_tutorial
   search_bar_page.click_menu
   log_in_page.click_log_in_page
-
 end
 
 When("I click on the login button") do
@@ -10,29 +9,25 @@ When("I click on the login button") do
 end
 
 Then("I am redirected and logged in") do
-  expect(log_in_page.isloggedin?).to eq("Ayman El'Harake")
+  expect(log_in_page.isloggedin?).to eq("Manav Pandey")
 end
-
-Given("I access the email sign in page") do
-
-end
-
-#@manavendu
 
 Given("I access the Google sign in page") do
   notes_page.skip_tutorial
   search_bar_page.click_menu
   log_in_page.click_log_in_page
   log_in_page.click_google_log_in
-  
+
+
  end
 
 When("I click on the gmail username") do
   log_in_page.click_sign_in
   log_in_page.click_on_email
-  log_in_page.click_on_email
-  log_in_page.click_on_email
+
   end
 
 
-
+  Then("I am am logged in") do
+    # Not possible because of the inspect error
+  end
