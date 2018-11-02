e0726064624016d19ea2707480cc50bbccc43b66
Daniel Eke <deke@spartaglobal.com>
2018-11-02 09:51:17 +0000

    Added initial trash test scenarios and step_defs file

---
+++ features/step_definitions/trash_step_defs.rb
@@ -0,0 +1,19 @@
+Given("I have deleted a note") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I navigate to the trash page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I restore the note from the navbar") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will be on an empty trash page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click permanently delete on the dropdown") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
--- features/trash.feature
+++ features/trash.feature
@@ -1,7 +1,17 @@
+@latest
 Feature: Trash Can
 
-  # Scenario: title
-  #   Given context
-  #   When event
-  #   Then outcome
-  #
+  Scenario: Restoring a deleted note
+    Given I have deleted a note
+    And I navigate to the trash page
+    When I click on the first note
+    And I restore the note from the navbar
+    And I click back
+    Then I will be on an empty trash page
+
+  Scenario: Permanently deleting a note
+    Given I have deleted a note
+    And I navigate to the trash page
+    When I long-click on the first note
+    And I click permanently delete on the dropdown
+    Then I will be on an empty trash page
